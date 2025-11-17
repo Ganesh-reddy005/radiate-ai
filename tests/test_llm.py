547a74f3e89c45f87ba64ffb56d80f8f02073b84
Ganesh Reddy <b.ganesh.reddy.05@gmail.com>
2025-11-17 15:34:05 +0530

    updated- test cases, smoker test's

--- tests/test_llm.py
+++ tests/test_llm.py
@@ -2,20 +2,21 @@ from radiate.llm import LLMClient
 from radiate.core import Radiate
 
 #calling the llm
-llm=LLMClient(provider="openrouter",api_key='KEY',
+llm=LLMClient(provider="openrouter",api_key='API_KEY',
               model='nvidia/nemotron-nano-12b-v2-vl:free')
 
 #using embedding model
 radiate=Radiate(embedding_provider='local')
+
 #ingesting our source 
-radiate.ingest('test_data/ml-book.pdf')
+radiate.ingest('test_data/')
 
 
-user_query='what is machine learning'
+user_query='How do i start a Business'
 
 #coding and decoding resonse
 result=radiate.query(user_query,mode='hybrid',top_k=3)
 
-llm_output=llm.answer(user_query,result['text'])
+llm_output=llm.answer(user_query,result)
 
 print(llm_output)
